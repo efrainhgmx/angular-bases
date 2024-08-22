13e290c535de0b7916ba3d8f9be7bea4dadc68c6
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-22 16:15:25 -0600

    Adding counter functionality in custom component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,28 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-counter-component',
-    template:'<h1>Hola Counter</h1>'
+    template:`<h1>{{ title }}</h1>
+    <h2>Counter: {{ counter }}</h2>
+    
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset Counter</button>
+    <button (click)="decreaseBy(1)">-1</button>`
 })
 export class CounterComponent{
+    public title: string   = 'Hello World';
+    public counter: number = 10;
+  
+    public increaseBy( value: number ):void {
+      this.counter += value;
+    }
+  
+    public decreaseBy( value: number):void {
+      this.counter -= value;
+    }
+  
+    public resetCounter():void {
+      this.counter = 10;
+    }
+
 
 }
