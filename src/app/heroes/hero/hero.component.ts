d8df8a36912fe1e8cb340a565b03537081e66eb2
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-22 16:50:31 -0600

    Adding boostrap and first structe in heroes component

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  public name: string = 'Iron Man';
+  public age: number  = 45;
+  
 }
