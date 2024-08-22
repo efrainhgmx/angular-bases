d2ddee3a23f98af40a8dcb51b8abb39f9ff94543
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-22 16:34:40 -0600

    Using CLI for component generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  public title: string   = 'Hello World';
-  public counter: number = 10;
-
-  public increaseBy( value: number ):void {
-    this.counter += value;
-  }
-
-  public decreaseBy( value: number):void {
-    this.counter -= value;
-  }
-
-  public resetCounter():void {
-    this.counter = 10;
-  }
+  
 }
