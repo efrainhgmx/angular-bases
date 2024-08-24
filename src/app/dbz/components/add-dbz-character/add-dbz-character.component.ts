485d838bcb2b772fde96f97bbff9ad23c75b448b
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-23 18:12:23 -0600

    Rename dbz component selector

--- src/app/dbz/components/add-dbz-character/add-dbz-character.component.ts
+++ src/app/dbz/components/add-dbz-character/add-dbz-character.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-add-dbz-character',
+  selector: 'dbz-add-character',
   templateUrl: './add-dbz-character.component.html',
   styleUrl: './add-dbz-character.component.css'
 })
