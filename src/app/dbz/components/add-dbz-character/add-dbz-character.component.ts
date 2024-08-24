e0f189b1f716697a249d842be35f7e8c481cc0c6
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-23 18:08:13 -0600

    Adding character component

---
+++ src/app/dbz/components/add-dbz-character/add-dbz-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-dbz-character',
+  templateUrl: './add-dbz-character.component.html',
+  styleUrl: './add-dbz-character.component.css'
+})
+export class AddDbzCharacterComponent {
+
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddDbzCharacterComponent } from './components/add-dbz-character/add-dbz-character.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListComponent
+    ListComponent,
+    AddDbzCharacterComponent
   ],
   imports: [
     CommonModule
