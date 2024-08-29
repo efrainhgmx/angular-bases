ede2e506fbf4019854d9c570a33768f06141fe22
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-29 11:58:46 -0600

    ngModel is an directive

--- src/app/dbz/components/add-dbz-character/add-dbz-character.component.ts
+++ src/app/dbz/components/add-dbz-character/add-dbz-character.component.ts
@@ -11,6 +11,8 @@ export class AddDbzCharacterComponent {
     @Output()
     public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
+    //* Las propiedades se pueden inicializar al momento de 
+    //* definirlas o en el contructor
     public character: Character = {
       name:'',
       power:0
