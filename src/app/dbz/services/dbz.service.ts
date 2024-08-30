f20743f5069d6b682425e1de0c9a68dd22708af2
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-30 13:35:13 -0600

    Using uuid for characters

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
     providedIn: 'root'
@@ -7,22 +8,27 @@ import { Character } from '../interfaces/character.interface';
 export class DbzService {
     public characters: Character[] = [
         {
+          id: uuid(),
           name: 'Krilin',
-          power: 500,
+          power: 500
         },
         {
+          id: uuid(),
           name: 'Goku',
           power: 9500,
         },
         {
+          id: uuid(),
           name: 'Vegeta',
           power: 7500,
         },
         {
+          id: uuid(),
           name: 'Cell',
           power: 10000,
         },
         {
+          id: uuid(),
           name: 'Roshi',
           power: 10,
         },
