0ad875af02f57298483d3647eb259c5b67952d58
Efrain Hernandez <efrainhg2050@gmail.com>
2024-08-30 13:10:23 -0600

    Adding format

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,38 +1,39 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Component({
-    selector: 'app-dbz-main-page',
-    templateUrl: './main-page.html'
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.html',
 })
-
-export class MainPageComponent  {
-   public characters: Character[] = [
+export class MainPageComponent {
+  public characters: Character[] = [
     {
-        name: 'Krilin',
-        power: 500
-    },{
-        name: 'Goku',
-        power: 9500
-    },{
-        name: 'Vegeta',
-        power: 7500
-    },{
-        name: 'Cell',
-        power: 10000
-    },{
-        name: 'Roshi',
-        power: 10
-    }
-   ];
+      name: 'Krilin',
+      power: 500,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+    {
+      name: 'Cell',
+      power: 10000,
+    },
+    {
+      name: 'Roshi',
+      power: 10,
+    },
+  ];
 
-   onNewCharacter(character: Character): void {
+  onNewCharacter(character: Character): void {
     this.characters.push(character);
-   }
+  }
 
-   onDeletedCharacter(index:number):void {
-    console.log({index})
-    this.characters.splice(index,1);
-    
-   }
+  onDeletedCharacter(index: number): void {
+    this.characters.splice(index, 1);
+  }
 }
